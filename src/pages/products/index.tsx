59f4105ebd65c421126628059291cc463d8cdefb
kifa-jp <kifa21@gmail.com>
2022-06-03 00:40:31 +0900

    linterの設定をnext.js用に変更

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -2,9 +2,9 @@ import { Box } from '@chakra-ui/react';
 
 const ProductsList = () => {
   return (
-    <Box display='flex' bg='tomato' w='100%' p={4} color='white'>
-      <main className=''>
-        <h2 className=''>商品一覧</h2>
+    <Box display="flex" bg="tomato" w="100%" p={4} color="white">
+      <main className="">
+        <h2 className="">商品一覧</h2>
       </main>
     </Box>
   );
