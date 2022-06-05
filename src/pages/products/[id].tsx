9a3bd67672c668e44787bebbcda4e75cfe68209f
kifa-jp <kifa21@gmail.com>
2022-06-06 01:23:26 +0900

    カテゴリごとの表示に対応

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -13,96 +13,137 @@ import NextLink from 'next/link';
 import Image from 'next/image';
 import { AiFillHeart, AiFillDislike } from 'react-icons/ai';
 
+type Params = {
+  id: Array<string>;
+};
+
+type RecipeList = {
+  result: Array<Recipe>;
+};
+
+type Recipe = {
+  foodImageUrl: string;
+  mediumImageUrl: string;
+  nickname: string;
+  pickup: number;
+  rank: string;
+  recipeConst: string;
+  recipeDescription: string;
+  recipeId: number;
+  recipeIndication: string;
+  recipeMaterial: Array<string>;
+  recipePublishday: string;
+  recipeTitle: string;
+  recipeUrl: string;
+  shop: number;
+  smallImageUrl: string;
+};
+
 // SSR
-export async function getServerSideProps({ params }: { params: any }) {
+export async function getServerSideProps({ params }: { params: Params }) {
+  // TODO: エラー発生時の再取得処理を実装(タイムアウト：1秒)
   const res = await fetch(
-    `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=${process.env.RAKUTEN_API_APP_ID}`,
+    `https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&categoryId=${params.id}&applicationId=${process.env.RAKUTEN_API_APP_ID}`,
   );
   const data = await res.json();
 
   return {
     props: {
-      product: data,
+      recipeList: data,
     },
   };
 }
 
-const Product = ({ product }: { product: any }) => {
+const Recipe = ({ recipeList }: { recipeList: RecipeList }) => {
   const router = useRouter();
-  const { id } = router.query;
-  return (
-    <Center bg={'gray.100'}>
-      <Center px={2} minH="100vh" w={['sm', 'md', 'lg', 'xl']} bg={'gray.100'}>
-        <Flex flexDir="column">
-          <Box textAlign={'right'} mb={4}>
-            <NextLink href="/products">
-              <Link>商品一覧へ</Link>
-            </NextLink>
-          </Box>
-          <Box
-            p={8}
-            mb={8}
-            boxShadow={'lg'}
-            textAlign="center"
-            rounded={6}
-            bg={'white'}
-          >
-            <Heading m={0} as="h2" fontSize={'xl'}>
-              {product.result[Number(id)].recipeTitle}
-            </Heading>
-            <Text>{product.result[Number(id)].recipeDescription}</Text>
-            <Image
-              src={product.result[Number(id)].foodImageUrl}
-              alt={product.result[Number(id)].recipeTitle}
-              width={600}
-              height={450}
-              objectFit="contain"
-            />
-          </Box>
-          <Flex w={'100%'} mb={4}>
-            <Spacer />
-            <Box w={'35%'} textAlign="center">
-              <Button
-                bg={'gray.500'}
-                color="white"
-                w={'100%'}
-                _hover={{ bg: 'gray.400' }}
-                _active={{ bg: 'gray.400' }}
-              >
-                <Box p={0.8}>
-                  <AiFillDislike />
-                </Box>
-                <Box as="span" ml={1} p={0.8}>
-                  Dislike
-                </Box>
-              </Button>
-            </Box>
-            <Spacer />
-            <Box w={'35%'} textAlign="center">
-              <NextLink href={`./${Number(id) + 1}`} passHref>
-                <Button
-                  bg={'red.500'}
-                  color="white"
-                  w={'100%'}
-                  _hover={{ bg: 'red.400' }}
-                  _active={{ bg: 'red.400' }}
-                  as="a"
-                >
-                  <Box p={0.8}>
-                    <AiFillHeart />
-                  </Box>
-                  <Box as="span" ml={1} p={0.8}>
-                    Like
-                  </Box>
-                </Button>
+  const { id, rank } = router.query;
+
+  if (!rank) {
+    return <Box>カテゴリトップ</Box>;
+  } else {
+    return (
+      <Center bg={'gray.100'}>
+        <Center
+          px={2}
+          minH="100vh"
+          w={['sm', 'md', 'lg', 'xl']}
+          bg={'gray.100'}
+        >
+          <Flex flexDir="column">
+            <Box textAlign={'right'} mb={4}>
+              <NextLink href="/products">
+                <Link>商品一覧へ</Link>
               </NextLink>
             </Box>
-            <Spacer />
+            <Box
+              p={8}
+              mb={8}
+              boxShadow={'lg'}
+              textAlign="center"
+              rounded={6}
+              bg={'white'}
+            >
+              <Heading m={0} as="h2" fontSize={'xl'}>
+                {console.log(recipeList)}
+                {recipeList.result[Number(rank)].recipeTitle}
+              </Heading>
+              <Text>{recipeList.result[Number(rank)].recipeDescription}</Text>
+              <Image
+                src={recipeList.result[Number(rank)].foodImageUrl}
+                alt={recipeList.result[Number(rank)].recipeTitle}
+                width={600}
+                height={450}
+                objectFit="contain"
+              />
+            </Box>
+            <Flex w={'100%'} mb={4}>
+              <Spacer />
+              <Box w={'35%'} textAlign="center">
+                <Button
+                  bg={'gray.500'}
+                  color="white"
+                  w={'100%'}
+                  _hover={{ bg: 'gray.400' }}
+                  _active={{ bg: 'gray.400' }}
+                >
+                  <Box p={0.8}>
+                    <AiFillDislike />
+                  </Box>
+                  <Box as="span" ml={1} p={0.8}>
+                    Dislike
+                  </Box>
+                </Button>
+              </Box>
+              <Spacer />
+              <Box w={'35%'} textAlign="center">
+                <NextLink
+                  href={`/products/${Number(id)}?rank=${Number(rank) + 1}`}
+                  passHref
+                >
+                  <Button
+                    bg={'red.500'}
+                    color="white"
+                    w={'100%'}
+                    _hover={{ bg: 'red.400' }}
+                    _active={{ bg: 'red.400' }}
+                    as="a"
+                  >
+                    <Box p={0.8}>
+                      <AiFillHeart />
+                    </Box>
+                    <Box as="span" ml={1} p={0.8}>
+                      Like
+                    </Box>
+                  </Button>
+                </NextLink>
+              </Box>
+              <Spacer />
+            </Flex>
           </Flex>
-        </Flex>
+        </Center>
       </Center>
-    </Center>
-  );
+    );
+  }
 };
 
-export default Product;
+export default Recipe;
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -5,10 +5,47 @@ import {
   ListItem,
   UnorderedList,
   Link,
+  Grid,
+  GridItem,
+  Box,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import * as fs from 'fs';
+import * as path from 'path';
 
-const ProductsList = () => {
+// SSR
+export async function getServerSideProps() {
+  const jsonPath = path.join(process.cwd(), 'public', 'category.json');
+  const jsonText = fs.readFileSync(jsonPath, 'utf-8');
+  const data = JSON.parse(jsonText);
+
+  return {
+    props: {
+      categoryList: data,
+    },
+  };
+}
+
+type CategoryList = {
+  large: Array<LargeCategory>;
+  medium: Array<MediumCategory>;
+};
+
+type LargeCategory = {
+  categoryId: string;
+  categoryName: string;
+  categoryUrl: string;
+};
+
+type MediumCategory = {
+  categoryId: number;
+  categoryName: string;
+  categoryUrl: string;
+  parentCategoryId: string;
+};
+
+// TODO: フォルダ名をproductsから変更
+const CategoryList = ({ categoryList }: { categoryList: CategoryList }) => {
   return (
     <Center px={2} minH="100vh" bg={'gray.100'}>
       <Flex
@@ -17,7 +54,7 @@ const ProductsList = () => {
         align="center"
         bg={'white'}
         rounded="6"
-        w={['sm', 'md', 'lg', 'xl']}
+        w={['350px', '640px', '800px', '960px']}
         boxShadow="lg"
       >
         <Heading
@@ -29,29 +66,27 @@ const ProductsList = () => {
           bg={'green.300'}
           roundedTop="6"
         >
-          レシピ一覧
+          カテゴリ一覧
         </Heading>
-
-        <UnorderedList w={'100%'} px={4}>
-          <ListItem>
-            <NextLink href="/products/0">
-              <Link>スマートフォン</Link>
-            </NextLink>
-          </ListItem>
-          <ListItem>
-            <NextLink href="/products/1">
-              <Link>パソコン</Link>
-            </NextLink>
-          </ListItem>
-          <ListItem>
-            <NextLink href="/products/2">
-              <Link>ヘッドホン</Link>
-            </NextLink>
-          </ListItem>
-        </UnorderedList>
+        <Grid
+          templateColumns={['repeat(2, 1fr)', 'repeat(3, 1fr)']}
+          gap={2}
+          px={4}
+        >
+          {categoryList.large.map((category) => (
+            <GridItem w={'100%'} key={category.categoryId} px={4}>
+              <NextLink
+                href={`/products/${category.categoryId}?rank=0`}
+                passHref
+              >
+                <Link>{category.categoryName}</Link>
+              </NextLink>
+            </GridItem>
+          ))}
+        </Grid>
       </Flex>
     </Center>
   );
 };
 
-export default ProductsList;
+export default CategoryList;
